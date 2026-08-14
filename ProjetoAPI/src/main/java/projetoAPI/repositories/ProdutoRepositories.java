4f689dbf0b933e41e0a9d9da1ce99d1dbe056f5a
Luis Gustavo <luisgscode@gmail.com>
2026-08-14 17:14:34 -0300

    fix: adiciona metodos de busca no ProdutoRepositories

--- ProjetoAPI/src/main/java/projetoAPI/repositories/ProdutoRepositories.java
+++ ProjetoAPI/src/main/java/projetoAPI/repositories/ProdutoRepositories.java
@@ -1,5 +1,23 @@
 package projetoAPI.repositories;
 
-public interface ProdutoRepositories {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import projetoAPI.entities.Produto;
+
+public interface ProdutoRepositories extends JpaRepository<Produto, Long> {
+
+	// Busca um produto pelo nome exato
+	Optional<Produto> findByNome(String nome);
+
+	// Busca produtos cujo nome contenha o texto informado (ignorando maiusculas/minusculas)
+	List<Produto> findByNomeContainingIgnoreCase(String nome);
+
+	//  produtos por categoria (caso a Entity tenha esse campo)
+	List<Produto> findByCategoria(String categoria);
+
+	//  produtos com quantidade em estoque abaixo do valor informado
+	List<Produto> findByQuantidadeLessThan(Integer quantidade);
 }
