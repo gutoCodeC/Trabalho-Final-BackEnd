a7e741dcd27ee2a510b23794a2dcd789109c88c3
Susana <susanaviannatech@gmail.com>
2026-08-15 21:48:52 -0300

    Restore Services de Usuario

---
+++ ProjetoAPI/src/main/java/projetoAPI/services/Services.java
@@ -0,0 +1,67 @@
+package projetoAPI.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import projetoAPI.entities.Usuario;
+import projetoAPI.repositories.Repositories;
+
+@Service
+public class Services {
+	private final Repositories repository;
+
+	public Services (Repositories repository) {
+		this.repository = repository;
+	}
+	
+	// Método para salvar um usuário no banco de dados
+	
+	public String SalvarUsuario	(Usuario usuario) {
+		repository.save(usuario);
+		return "Usuário cadastrado com sucesso!";
+	}
+	
+	// Método para localizar um usuário pelo ID
+	
+	public Usuario buscaId(Long id) {
+		return repository.findById(id).orElseThrow(
+				() -> new RuntimeException ("Usuário não encontrado"));
+		}
+	
+	// Método para localizar um usuário pelo Email
+	
+	public Usuario buscaEmail(String email) {
+		return repository.findByEmail(email).orElseThrow(
+				() -> new RuntimeException ("Usuário não encontrado"));
+	}
+
+	// Método para localizar todos os usuários
+	public List<Usuario> procurarTodos() {
+		return repository.findAll();
+	}
+	
+	// Método para deletar um usuário pelo Id
+	public String deletarPorId (Long id){
+		repository.deleteById(id);
+		return "Usuário deletado com sucesso";
+	}
+	
+	// Método para deletar um usuário pelo email
+		public String deletarPorEmail (String email) {
+			repository.deleteByEmail(email);
+			return "Usuário deletado com sucesso";
+		}
+		
+		public String editarUsuario (Long id, Usuario usuario) {
+			Usuario response = repository.findById(id).get();
+			
+			response.setNome(usuario.getNome());
+			response.setEmail(usuario.getEmail());
+			response.setSenha(usuario.getSenha());
+			
+			repository.save(response);
+			return "Usuário editado com sucesso!!!";
+		}
+		
+	}
